13803b7c9cf8a83bbcd364411ebd156f6d13a871
Sebastian Le <sebastianle296@gmail.com>
2024-06-12 14:53:57 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes 
+app.use('/api/auth', require('./server/routes/auth'));
+
+app.get('/', (req, res) => {
+  res.send('Culinary Compass API');
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
